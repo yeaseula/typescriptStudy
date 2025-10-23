6e5fcd534c660343a52043044166f02b2a8a5106
yeaseula <kminea@naver.com>
2025-10-23 15:15:10 +0900

    사용자 정의 타입가드

---
+++ typescript-func/src/chapter5.ts
@@ -0,0 +1,34 @@
+/**
+ * 사용자 정의 타입 가드
+ */
+
+type Cat = {
+    name:string,
+    isScratch: boolean
+}
+
+type Dog = {
+    name:string,
+    isBark: boolean
+}
+
+type Animal = Dog | Cat;
+
+//이렇게 타입가드를 직접 만들 수 있습니다.
+function isDog(animal:Animal):animal is Dog {
+    return (animal as Dog).isBark !== undefined
+}
+
+function isCat(animal:Animal):animal is Cat {
+    return (animal as Cat).isScratch !== undefined
+}
+
+function warning(animal:Animal) {
+    if(isDog(animal)) {
+        //강아지
+        animal
+    } else if (isCat(animal)) {
+        //고양이
+        animal
+    }
+}
